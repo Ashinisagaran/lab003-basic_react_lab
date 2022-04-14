1be331f4e0222a5315c91137cc94b676423babc9
Ashini Sagaran <20087371@mail.wit.ie>
2022-04-14 13:08:51 +0100

    Exercise 1 completed.

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,7 +3,50 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>BSc Computer Forensic and Security - Modules table </h2>
+      <table className="table table-bordered">
+        <thead> 
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+           <td>WAD 2</td>
+           <td>2</td>
+           <td>2</td>
+          </tr>
+          <tr>
+           <td>LPoCFS</td>
+           <td>4</td>
+           <td>0</td>
+          </tr>
+          <tr>
+           <td>NF</td>
+           <td>2</td>
+           <td>2</td>
+          </tr>
+          <tr>
+           <td>DO</td>
+           <td>1</td>
+           <td>3</td>
+          </tr>
+          <tr>
+           <td>PP</td>
+           <td>3</td>
+           <td>0</td>
+          </tr>
+          <tr>
+           <td>NoSQL D</td>
+           <td>2</td>
+           <td>2</td>
+          </tr>
+        </tbody>  
+      </table>
+    </div>
   );
 };
 
